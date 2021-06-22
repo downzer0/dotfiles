4cc5b7e05d1370064444fb41512c28e06aebd41b
Tony Kornmeier <akornmeier@users.noreply.github.com>
2021-06-22 13:52:41 -0400

    chore: remove grc alias for make (#7)

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,5 +1,7 @@
 # GRC colorizes nifty unix tools all over the place
 if (( $+commands[grc] )) && (( $+commands[brew] ))
 then
-  source `brew --prefix`/etc/grc.bashrc
+  source `brew --prefix`/etc/grc.sh
+  unalias make
+  alias make='/usr/bin/make'
 fi
